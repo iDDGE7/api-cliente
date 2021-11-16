4888a6ad84150424d42eb1a3e824b7459d157127
Edgar de Dios <eduardoddg60@gmail.com>
2021-11-15 22:12:17 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+const API_LINK = process.env.REACT_APP_API_URI;
+const App = () => {
+  const [currentCookie, setCurrentCookie] = useState('no cookie');
 
-function App() {
+  const setCookie = () => {
+    axios.get(`${API_LINK}/setCookie`, {
+      withCredentials: true,
+    }).then((response) => {
+      setCurrentCookie(response.data.cookie);
+  });;
+  };
+
+  const deleteCookie = () => {
+    axios.get(`${API_LINK}/deleteCookie`, {
+      withCredentials: true,
+    }).then((response) => {
+      setCurrentCookie('');
+  });;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>welcome to cookie test</h1>
+      <p>{currentCookie}</p>
+      <br />
+      <button onClick={setCookie}>set cookie</button>
+      <br />
+      <br />
+      <button onClick={deleteCookie}>delete cookie</button>
     </div>
   );
-}
+};
 
 export default App;

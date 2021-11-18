c8ac4d98794af78161fec66fc7225fa7ca047f4f
Edgar de Dios <eduardoddg60@gmail.com>
2021-11-17 22:30:21 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 const API_LINK = process.env.REACT_APP_API_URI;
 const App = () => {
-  const [currentCookie, setCurrentCookie] = useState('no cookie');
+     const [currentCookie, setCurrentCookie] = useState("");
 
-  const setCookie = () => {
-    axios.get(`${API_LINK}/setCookie`, {
-      withCredentials: true,
-    }).then((response) => {
-      setCurrentCookie(response.data.cookie);
-  });;
-  };
+     useEffect(() => {
+          checkCookie();
+     }, []);
 
-  const deleteCookie = () => {
-    axios.get(`${API_LINK}/deleteCookie`, {
-      withCredentials: true,
-    }).then((response) => {
-      setCurrentCookie('');
-  });;
-  };
+     const setCookie = () => {
+          axios.get(`${API_LINK}/setCookie`, {
+               withCredentials: true,
+          }).then((response) => {
+               checkCookie();
+          });
+     };
 
-  return (
-    <div className="App">
-      <h1>welcome to cookie test</h1>
-      <p>{currentCookie}</p>
-      <br />
-      <button onClick={setCookie}>set cookie</button>
-      <br />
-      <br />
-      <button onClick={deleteCookie}>delete cookie</button>
-    </div>
-  );
+     const deleteCookie = () => {
+          axios.get(`${API_LINK}/deleteCookie`, {
+               withCredentials: true,
+          }).then((response) => {
+               checkCookie();
+          });
+     };
+
+     const checkCookie = () => {
+          axios.get(`${API_LINK}/checkCookie`, {
+               withCredentials: true,
+          }).then((response) => {
+               if (response.data.error) {
+                    setCurrentCookie(response.data.error);
+               } else {
+                    setCurrentCookie(response.data.cookie);
+               }
+          });
+     };
+
+     return (
+          <div className="App">
+               <h1>inicio</h1>
+               <br />
+               <p>{currentCookie}</p>
+               <br />
+               <button onClick={setCookie}>set cookie</button>
+               <br />
+               <br />
+               <button onClick={deleteCookie}>delete cookie</button>
+          </div>
+     );
 };
 
 export default App;
